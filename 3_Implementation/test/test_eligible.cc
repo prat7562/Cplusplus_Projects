a640b877b6af7a7d5b61d060c19c4691e12047a7
256623 <66222498+Manojna52@users.noreply.github.com>
2021-09-04 23:19:27 +0530

    update

---
+++ 3_Implementation/test/test_eligible.cc
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<vector>
+#include<fstream>
+#include<string.h>
+#include<sstream>
+#include <time.h>
+#include<chrono>
+#include<algorithm>
+#include<map>
+#include"score.h"
+#include<gtest/gtest.h>
+using namespace std;
+
+
+TEST(eligibile_true,score)
+{
+    /*Student(int u_id,std::string name,std::string m1_name,int m1_score,std::string m2_name,int m2_score,std::string m3_name,int m3_score*/
+int i_uid=123;
+string name_1="man";
+string module_name1="pyth";
+int i_marks_m1=98;
+string module_name2="c++";
+int i_marks_m2=75;
+string module_name3="sdlc";
+int i_marks_m3=76;
+Student s2(i_uid,name_1,module_name1,i_marks_m1,module_name2,i_marks_m2,module_name3,i_marks_m3);
+EXPECT_TRUE(s2.check_eligibility());
+int ii_uid=123;
+string iname_1="man";
+string imodule_name1="pyth";
+int ii_marks_m1=91;
+string imodule_name2="c++";
+int ii_marks_m2=43;
+string imodule_name3="sdlc";
+int ii_marks_m3=83;
+Student s1(ii_uid,iname_1,imodule_name1,ii_marks_m1,imodule_name2,ii_marks_m2,imodule_name3,ii_marks_m3);
+EXPECT_TRUE(s1.check_eligibility());
+int u_uid=123;
+string uname_1="man";
+string umodule_name1="pyth";
+int ui_marks_m1=93;
+string umodule_name2="c++";
+int ui_marks_m2=49;
+string umodule_name3="sdlc";
+int ui_marks_m3=73;
+Student s3(u_uid,uname_1,umodule_name1,ui_marks_m1,umodule_name2,ui_marks_m2,umodule_name3,ui_marks_m3);
+EXPECT_TRUE(s3.check_eligibility());
+
+
+
+
+}
+TEST(eligible_flase,False)
+{
+  int i_uid=123;
+string name_1="man";
+string module_name1="pyth";
+int i_marks_m1=41;
+string module_name2="c++";
+int i_marks_m2=41;
+string module_name3="sdlc";
+int i_marks_m3=41;
+Student s2(i_uid,name_1,module_name1,i_marks_m1,module_name2,i_marks_m2,module_name3,i_marks_m3);
+EXPECT_FALSE(s2.check_eligibility());
+int ii_uid=123;
+string iname_1="man";
+string imodule_name1="pyth";
+int ii_marks_m1=41;
+string imodule_name2="c++";
+int ii_marks_m2=43;
+string imodule_name3="sdlc";
+int ii_marks_m3=32;
+Student s1(ii_uid,iname_1,imodule_name1,ii_marks_m1,imodule_name2,ii_marks_m2,imodule_name3,ii_marks_m3);
+EXPECT_FALSE(s1.check_eligibility());
+int u_uid=123;
+string uname_1="man";
+string umodule_name1="pyth";
+int ui_marks_m1=4;
+string umodule_name2="c++";
+int ui_marks_m2=4;
+string umodule_name3="sdlc";
+int ui_marks_m3=7;
+Student s3(u_uid,uname_1,umodule_name1,ui_marks_m1,umodule_name2,ui_marks_m2,umodule_name3,ui_marks_m3);
+EXPECT_FALSE(s3.check_eligibility());  
+}
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
